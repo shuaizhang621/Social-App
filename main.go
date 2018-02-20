622691fc645b62ce4ab358dacc1c5aef56209a42
ShuaiZhang <sz1950@nyu.edu>
2018-02-20 16:55:22 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.196.174.255:9200"
+	ES_URL = "http://35.196.126.212:9200"
 )
 
 func main() {
